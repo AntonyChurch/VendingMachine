33dbaf95cb3a7afc2afdbfb0238500a6082857cb
Antony Church <achurch44.ac@gmail.com>
2017-05-12 12:32:29 +0100

    Added comments for reason behind custom session handler

--- VendingMachine/Services/ISessionService.cs
+++ VendingMachine/Services/ISessionService.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace VendingMachine.Services
 {
+    // .Net Core ASP.NET Core has a built in session service,
+    // however it was not functioning on my Linux install,
+    // so I have had to hand roll a simple one.
     public interface ISessionService
     {
         void StoreCurrentTally(Guid sessionId, double currentValue);
